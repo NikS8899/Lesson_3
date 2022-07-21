e7304fb7b67abd3fa908d6b9f2f8471afdfcfce2
NikitaS <sergio791309@gmail.com>
2022-07-21 20:18:16 +0300

    Create task 2 for lesson 3

---
+++ Task_2/Task_2.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// формула: AB = √(x1 - y1) ^ 2 + (x2 - y2) ^ 2 + (x3 - y3) ^ 2
+
+Console.WriteLine("В порядке очереди (x 1; y 1; z 1) введите координаты точки A:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В порядке очереди (x 2; y 2; z 2) введите координаты точки B:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+var results = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2));
+Console.WriteLine("Расстояние между точками: " + Math.Round(results));
